470ae111faf0633f0bba46a9410dedfb36485687
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2026-04-14 11:12:28 +0300

    refactor(up.sql): изменено название и constraint

--- balance-keeper-service/migrations/000001_create_accounts_table.up.sql
+++ balance-keeper-service/migrations/000001_create_accounts_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS accounts (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    value DECIMAL(12, 2) NOT NULL,
+    balance DECIMAL(12, 2) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    CONSTRAINT accounts_dates_check CHECK (updated_at >= created_at)
+    CONSTRAINT accounts_dates_check CHECK (updated_at >= created_at),
+    CONSTRAINT accounts_balance_check CHECK (balance <= 1000000000.00)
 );
 
 CREATE INDEX IF NOT EXISTS idx_accounts_updated_at ON accounts(updated_at);
